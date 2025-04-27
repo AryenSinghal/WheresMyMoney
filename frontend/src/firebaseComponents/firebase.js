61fc07a9da3365f561c9daa9dff3144a88d743a8
Aryen Singhal <aryensinghal@gmail.com>
2025-04-26 17:49:33 -0700

    revert env variables thing to hardcoded values

--- frontend/src/firebaseComponents/firebase.js
+++ frontend/src/firebaseComponents/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyCm3Un8qa7Hxll_11B7_oae0kWG3ZOap2c",
+    authDomain: "wheresmymoney-a39c9.firebaseapp.com",
+    projectId: "wheresmymoney-a39c9",
+    storageBucket: "wheresmymoney-a39c9.firebasestorage.app",
+    messagingSenderId: "542739432911",
+    appId: "1:542739432911:web:6da6a92eedcd37972d7eba"
 };
 
 const app = initializeApp(firebaseConfig);
